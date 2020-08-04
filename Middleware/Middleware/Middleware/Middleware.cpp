b9ecb0a28c828afa23b5eb0ff0a0f928df54dc03
trevinofernando <trevino.96@hotmail.com>
2020-08-04 14:12:40 -0400

    Vector2 struct implemented

--- Middleware/Middleware/Middleware/Middleware.cpp
+++ Middleware/Middleware/Middleware/Middleware.cpp
@@ -23,17 +23,13 @@ struct Vector3
 
 	inline Vector3 operator ++ (int)
 	{
-		x += 1;
-		y += 1;
-		z += 1;
+		x += 1; y += 1; z += 1;
 		return *this;
 	}
 
 	inline Vector3 operator += (const Vector3& V)
 	{
-		x += V.x;
-		y += V.y;
-		z += V.z;
+		x += V.x; y += V.y; z += V.z;
 		return *this;
 	}
 
@@ -59,9 +55,7 @@ struct Vector3
 
 	inline Vector3 operator = (const Vector3& V) 
 	{
-		x = V.x;
-		y = V.y;
-		z = V.z;
+		x = V.x; y = V.y; z = V.z;
 		return V;
 	}
 
@@ -76,6 +70,70 @@ struct Vector3
 	}
 };
 
+struct Vector2
+{
+	float x = 0, y = 0;
+
+	inline Vector2(void) {}
+	inline Vector2(const float X, const float Y, const float Z)
+	{
+		x = X; y = Y;
+	}
+
+	inline Vector2 operator + (const float C) const
+	{
+		return Vector2(x + C, y + C, z + C);
+	}
+
+	inline Vector2 operator ++ (int)
+	{
+		x += 1; y += 1;
+		return *this;
+	}
+
+	inline Vector2 operator += (const Vector2& V)
+	{
+		x += V.x; y += V.y;
+		return *this;
+	}
+
+	inline Vector2 operator * (const float C) const
+	{
+		return Vector2(x * C, y * C);
+	}
+
+	inline Vector2 operator - (const float C) const
+	{
+		return Vector2(x - C, y - C);
+	}
+
+	inline Vector2 operator + (const Vector2& V) const
+	{
+		return Vector2(x + V.x, y + V.y);
+	}
+
+	inline Vector2 operator - (const Vector2& V) const
+	{
+		return Vector2(x - V.x, y - V.y);
+	}
+
+	inline Vector2 operator = (const Vector2& V)
+	{
+		x = V.x; y = V.y;
+		return V;
+	}
+
+	inline bool operator == (const Vector2& V) const
+	{
+		return (x == V.x && y == V.y);
+	}
+
+	inline float Dot(const Vector2& V) const
+	{
+		return V.x * x + V.y * y;
+	}
+};
+
 
 float FPStoMilliseconds(unsigned int fps);
 void timer_start(std::function<void(void)> func, unsigned int interval);
@@ -85,16 +143,6 @@ void FixedUpdate();
 
 int main()
 {
-	struct Vector3 myVector1 = Vector3(1, 2, 3);
-	struct Vector3 myVector2 = Vector3(1, -2, 1);
-	struct Vector3 myVector = myVector1;
-	myVector++;
-	//myVector++;
-	std::cout << "2 " << myVector.x  << std::endl;
-	std::cout << "3 " << myVector.y  << std::endl;
-	std::cout << "4 " << myVector.z  << std::endl;
-
-
 	
 	timer_start(FixedUpdate, FPStoMilliseconds(FPS));
 	
