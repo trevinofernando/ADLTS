ad99e39e41b5787f70fd4d92087707bc3c000738
Aakash Gupta <gp.aakash@gmail.com>
2020-09-14 22:56:51 -0400

    Update motors.cpp

--- Raspberry Pi Motor Controls/Two Motor controls/motors.cpp
+++ Raspberry Pi Motor Controls/Two Motor controls/motors.cpp
@@ -1,14 +1,24 @@
 #include "motors.h"
 
+/*
 StepperMotors::StepperMotors()
 {
   wiringPiSetup();
   this->myStepper1 = new Stepper(StepsPerRevolution, connectionMotor1, connectionMotor2);
   this->myStepper2 = new Stepper(StepsPerRevolution, connectionMotor3, connectionMotor4);
 }
+*/
+StepperMotors::StepperMotors(): myStepper1(StepsPerRevolution, connectionMotor1, connectionMotor2), myStepper2(StepsPerRevolution, connectionMotor3, connectionMotor4)
+{
+}
 
 void StepperMotors::step(float angleX, float angleY)
 {
   this->myStepper1.stepAngle(angleX, StepsPerRevolution);
   this->myStepper2.stepAngle(angleY, StepsPerRevolution);
+}
+
+
+StepperMotors::~StepperMotors()
+{
 }
