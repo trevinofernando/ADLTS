20fd38b182daf0d90bac66fcca418e13ba7a0e05
Aakash Gupta <gp.aakash@gmail.com>
2020-09-14 22:27:33 -0400

    Update mainFile.cpp

--- Raspberry Pi Motor Controls/mainFile.cpp
+++ Raspberry Pi Motor Controls/mainFile.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "motor.h"
-
-#define stepsPerRevolution 1600
+/*
+#define totalStepsPerRevolution 1600
 #define connectionMotor1 0
 #define connectionMotor2 2
 #define connectionMotor3 3
 #define connectionMotor4 4
+*/
+
 using namespace std;
 
 
@@ -14,7 +16,7 @@ int main()
     float angle;
     
     wiringPiSetup();
-    Stepper myStepper(stepsPerRevolution, connectionMotor1, connectionMotor2);
+    Stepper myStepper(totalStepsPerRevolution, connectionMotor1, connectionMotor2);
     myStepper.stepAngle(angle, stepsPerRevolution);
     
     return 0;
