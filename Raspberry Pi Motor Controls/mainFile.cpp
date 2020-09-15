06417e4bdc3bff39405bb6ed51066d3ad13a6ddb
Aakash Gupta <gp.aakash@gmail.com>
2020-09-14 22:17:13 -0400

    Update mainFile.cpp

--- Raspberry Pi Motor Controls/mainFile.cpp
+++ Raspberry Pi Motor Controls/mainFile.cpp
@@ -1,1 +1,15 @@
+int main()
+{
+    float angle;
+    
+    int stepsPerRevolution = 1600;
+    wiringPiSetup();
+    Stepper myStepper(stepsPerRevolution, 0, 2);
+    
+    cin>>angle;
+        void Stepper::stepAngle(float angles_to_move, int stepsPerRevolution)
+    myStepper.stepAngle(angle, stepsPerRevolution);
+    
+    return 0;
+}
 
