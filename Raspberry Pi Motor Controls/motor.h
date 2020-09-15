7915e0d0c6bf75490a263311886299e2e03921b7
Aakash Gupta <gp.aakash@gmail.com>
2020-09-14 22:05:39 -0400

    Create motor.h

---
+++ Raspberry Pi Motor Controls/motor.h
@@ -0,0 +1,30 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+class Stepper {
+  public:
+    // constructors:
+    Stepper(int number_of_steps, int motor_pin_1, int motor_pin_2);
+
+    // speed setter method:
+    void setSpeed(long whatSpeed);
+
+    // mover method:
+    void step(int number_of_steps);
+
+  private:
+    void stepMotor(int this_step);
+
+    int direction;            // Direction of rotation
+    unsigned long step_delay; // delay between steps, in ms, based on speed
+    int number_of_steps;      // total number of steps this motor can take
+    int step_number;          // which step the motor is on
+
+    // motor pin numbers:
+    int motor_pin_1;
+    int motor_pin_2;
+
+    unsigned long last_step_time; // time stamp in us of when the last step was taken
+};
+
+#endif
