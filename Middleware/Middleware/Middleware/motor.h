fb57c17495ff1b0db1c1c57c30306506aa043264
Aakash Gupta <gp.aakash@gmail.com>
2020-09-18 00:51:20 -0400

    Add files via upload

---
+++ Middleware/Middleware/Middleware/motor.h
@@ -0,0 +1,57 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#define StepsPerRevolution 1600
+#define connectionMotor1 0
+#define connectionMotor2 2
+#define connectionMotor3 3
+#define connectionMotor4 4
+
+
+#include "Vectors.h"
+
+
+class Stepper {
+  public:
+    // constructors:
+    Stepper(int number_of_steps, int motor_pin_1, int motor_pin_2);
+    ~Stepper();
+
+    // speed setter method:
+    void setSpeed(long whatSpeed);
+
+    // mover method:
+    void step(int number_of_steps);
+    void stepAngle(float angles_to_move, int stepsPerRevolution);
+
+  private:
+    void stepMotor(int this_step);
+
+    int direction;            // Direction of rotation
+    unsigned long step_delay; // delay between steps, in ms, based on speed
+    int number_of_steps;      // total number of steps this motor can take
+    int step_number;          // which step the motor is on
+
+    // motor pin numbers:
+    int motor_pin_1;
+    int motor_pin_2;
+
+    unsigned long last_step_time; // time stamp in us of when the last step was taken
+};
+
+
+
+class StepperMotors {
+  public:
+    // constructors:
+    StepperMotors();
+    ~StepperMotors();
+
+    // mover method:
+    void step(Vector2 v);
+
+  private:
+    Stepper myStepper1;
+    Stepper myStepper2;
+};
+#endif
